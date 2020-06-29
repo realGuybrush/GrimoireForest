45b1650fce626114843deac6b54d0e2c02b3c397
realGuybrush <TripleBaka@yandex.ru>
2020-06-29 11:33:28 +0300

    Fixed bug of previous commit: environment prefabs and pictures added.

---
+++ Unity/Assets/MyAssets/Scripts/BackGroundMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMovement : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public float movementCoefficient = 1.0f;
+    public float tileWidth = 25.0f;
+    public int tileOffset = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetPosition();
+    }
+
+    public void SetPosition()
+    {
+        this.gameObject.transform.position = new Vector3((mainCamera.transform.position.x + tileOffset*tileWidth)*movementCoefficient, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+    }
+}
